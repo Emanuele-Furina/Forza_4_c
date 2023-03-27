ebfee883f49124b7031aa97724e28d02f5057677
James Rampling <122362864+Rampling-James@users.noreply.github.com>
2023-03-27 08:45:04 +0200

    redefine cell type

--- definitions.h
+++ definitions.h
@@ -4,9 +4,14 @@
 #define BOARD_WIDTH 7
 #define BOARD_HEIGHT 6
 
-typedef char CellState;
-#define STATE_EMPTY ((CellState) 0)
-#define STATE_PLAYER1 ((CellState) 1)
-#define STATE_PLAYER2 ((CellState) 2)
+typedef enum {
+	CELL_EMPTY,
+	CELL_PLAYER1,
+	CELL_PLAYER2,
 
-#endif
+	CELL_STATE_LENGTH
+} CellState;
+
+typedef CellState Board[BOARD_WIDTH][BOARD_HEIGHT];
+
+#endif /* F4_DEFINITIONS_H */
--- forza_quattro.c
+++ forza_quattro.c
@@ -3,6 +3,18 @@
 
 #include "definitions.h"
 
+int board_available_in_column(Board b, int column) {
+	if (column < 0 || column >= BOARD_WIDTH)
+		return -1;
+
+	for (int y = 0; y < BOARD_HEIGHT; y++)
+		if (b[column][y] != CELL_EMPTY) return y - 1;
+	return BOARD_HEIGHT - 1;
+}
+
 int main(void) {
+	Board game = { 0 };
+
+
 	return 0;
 }
